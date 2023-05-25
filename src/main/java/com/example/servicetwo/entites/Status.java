4b2d7cfabb9c53c0a1ca48a751a634592f224e08
Shamil <shaydulov@arcsinus.ru>
2023-05-25 10:34:51 +0300

    Коррекнтаяя реализаци получения сообщений из стрима реббита

--- src/main/java/com/example/servicetwo/entites/Status.java
+++ src/main/java/com/example/servicetwo/entites/Status.java
@@ -3,11 +3,9 @@ package com.example.servicetwo.entites;
 import javax.persistence.*;
 
 import lombok.Builder;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
 @Builder
 @Entity
 @Table(name = "statuses")
@@ -26,11 +24,54 @@ public class Status {
     @Column(name = "time_stamp")
     private LocalDateTime time_stamp;
 
+    public Status() {
+
+    }
+
+    public Status(Long id, Long serviceId, String serviceStatus, LocalDateTime time_stamp) {
+        this.id = id;
+        this.serviceId = serviceId;
+        this.serviceStatus = serviceStatus;
+        this.time_stamp = time_stamp;
+    }
+
     public static Status fromMessageModel(MessageModel messageModel) {
         return Status.builder()
                 .serviceId(messageModel.getServiceId())
                 .serviceStatus(messageModel.getStatus())
                 .time_stamp(LocalDateTime.now())
                 .build();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getServiceStatus() {
+        return serviceStatus;
+    }
+
+    public void setServiceStatus(String serviceStatus) {
+        this.serviceStatus = serviceStatus;
+    }
+
+    public LocalDateTime getTime_stamp() {
+        return time_stamp;
+    }
+
+    public void setTime_stamp(LocalDateTime time_stamp) {
+        this.time_stamp = time_stamp;
     }
 }
